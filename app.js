3de916e524af9db87d0f68696d76999a3d83f2aa
casperboegharb22-collab <casper.boegharb22@gmail.com>
2026-07-25 21:39:22 +0200

    Add initial product list to app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+const products = [
+  {
+    name: "Blue Razz Ice / Strawberry Watermelon Bubble Gum",
+    stock: 0
+  },
+  {
+    name: "Blueberry Ice / Love 66",
+    stock: 0
+  },
+  {
+    name: "Strawberry Watermelon / Grape Ice",
+    stock: 0
+  },
+  {
+    name: "Lemon Lime / Lush Ice",
+    stock: 0
+  }
+];
